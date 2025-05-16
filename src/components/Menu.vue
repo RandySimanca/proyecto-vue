5172fa18e95bdb924a6bac76e575e11d60d0925f
Randy Simanca <randysimancamercado@gmail.com>
2025-05-15 22:51:16 -0500

    componente lista de tareas, borrar componente autogenerados y se crean rutas

---
+++ src/components/Menu.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="menu">
+        <h2>Menú</h2>
+        <ul>
+        <li><router-link to="/Contador">Contador</router-link></li>
+        <li><router-link to="/ListaDeTareas">Lista de Tareas</router-link></li>
+        </ul>
+    </div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+.menu{
+    width: 200px;
+    background-color: rgb(177, 223, 250);
+    padding: 20px;
+}
+
+</style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <Menu></Menu>
+        <div class="content">
+            <router-view></router-view>
+
+        </div>
+
+    </div>
+
+</template>
+
+<script setup>
+import Menu from '../components/Menu.vue';
+
+</script>
+
+<style scoped>
+.comntainer {
+    display: flex;
+}
+.content {
+    flex: 1;
+    padding: 20px;
+}
+
+</style>
